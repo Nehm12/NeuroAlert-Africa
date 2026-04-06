6dd13fb78a88ac518ec3a0616ae54ce1a7d6b742
lesinfosspecial-wq <lesinfosspecial@gmail.com>
2026-04-06 16:22:39 +0200

    Ajout du module IA (modelIA-emmanuel)

---
+++ modelIA/__init__.py
@@ -0,0 +1,6 @@
+"""
+Module IA NeuroAlert Africa.
+
+Import explicite recommandé pour éviter les cycles d'import :
+    from modelIA.main import NeuroAlertModel, FASTInput, StrokeAnalysisResult
+"""
