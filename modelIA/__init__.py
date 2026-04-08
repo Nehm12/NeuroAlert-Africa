7a61f1791a4ef63b9d355c76aa3a84bf9c77b806
Nehm12 <houngajunior72@gmail.com>
2026-04-08 19:01:04 +0100

    deleting anual files

--- modelIA/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Module IA NeuroAlert Africa.
-
-Import explicite recommandé pour éviter les cycles d'import :
-    from modelIA.main import NeuroAlertModel, FASTInput, StrokeAnalysisResult
-"""
